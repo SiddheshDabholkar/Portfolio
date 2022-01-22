9e931c76af7f88a4944de2e05683d406950af94a
siddhesh <iamsiddhesh22@gmail.com>
2022-01-22 16:43:29 +0530

    added navbar

--- Layout/Navbar.jsx
+++ Layout/Navbar.jsx
@@ -1,5 +1,31 @@
 import styles from "./Navbar.module.scss";
+import { Link as SLink } from "react-scroll";
+
+const Routes = [
+  { id: 1, name: "Home", to: "Home" },
+  { id: 2, name: "About", to: "About" },
+  { id: 3, name: "Projects", to: "Projects" },
+  { id: 4, name: "Contact", to: "Contact" },
+];
 
 export default function Navbar() {
-  return <div className={styles.Navbar}>Navbar</div>;
+  return (
+    <div className={styles.Navbar}>
+      <div className={styles.NavbarLogo}>navbar</div>
+      <div className={styles.NavbarRoutes}>
+        {Routes.map((r) => (
+          <SLink
+            to={r.to}
+            spy={true}
+            smooth={true}
+            offset={1}
+            duration={500}
+            className={styles.NavbarRoutesItem}
+          >
+            {r.name}
+          </SLink>
+        ))}
+      </div>
+    </div>
+  );
 }
