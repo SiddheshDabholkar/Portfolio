0c950ebedbb952a1a6dccc2c75a62ee748202836
siddhesh <iamsiddhesh22@gmail.com>
2022-01-25 23:36:51 +0530

    changes svg size

--- section/Project.jsx
+++ section/Project.jsx
@@ -1,26 +1,49 @@
 import React from "react";
 import styles from "./Project.module.scss";
 import Image from "next/image";
+import Link from "next/link";
+import { BiLinkExternal } from "react-icons/bi";
+import { BsGithub } from "react-icons/bs";
 
-import one from "/public/static/projects/one.jpg";
 import RotatingDivs from "../components/RotatingDivs";
-
-const data = [{ id: 1, img: one, name: "Just" }];
+import Github from "/public/static/icons/icons8-github.svg";
+import { projectsData } from "../data/ProjectData";
 
 export default function Project() {
   return (
     <div className={styles.Project} id="Project">
       <span>Project</span>
-      {/* <h1>Something I Have Worked On</h1> */}
       <div className={styles.ProjectCont}>
-        {data.map((d) => (
-          <div className={styles.ProjectContRow}>
+        {projectsData.map((d) => (
+          <div key={d.id} className={styles.ProjectContRow}>
             <div className={styles.ProjectContRowImg}>
-              <Image src={d.img} className={styles.ProjectContRowImgStyle} />
+              <Image src={d.image} className={styles.ProjectContRowImgStyle} />
             </div>
             <div className={styles.ProjectContRowDes}>
-              <RotatingDivs />
-              <div className={styles.ProjectContRowDes4}>{d.name}</div>
+              {/* <RotatingDivs /> */}
+              <div className={styles.ProjectContRowDes4}>
+                <div>
+                  <span>{d.name}</span>
+                </div>
+                <div>
+                  <p>{d.description}</p>
+                </div>
+                <div className={styles.ProjectContRowDes4Tech}>
+                  {console.log(d.technologies.map((t) => t.icon))}
+                  {d.technologies.map((t) => {
+                    const Icon = t.icon;
+                    return <Icon className={styles.ProjectContRowDes4TechIcon}/>;
+                  })}
+                </div>
+                <div>
+                  <Link href={d.githubLink}>
+                    <BsGithub />
+                  </Link>
+                  <Link href={d.liveLink}>
+                    <BiLinkExternal />
+                  </Link>
+                </div>
+              </div>
             </div>
           </div>
         ))}
